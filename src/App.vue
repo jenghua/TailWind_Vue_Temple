6c86264c5f8be9463dc68d62bbdbef08df03f5ca
jenghua <zheng41457@gmail.com>
2020-08-27 11:37:21 +0800

    TailWind_Vue_Template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mycat value="Number of cats"></mycat>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/css/main.css'
+import helloWorld from './components/HelloWorld.vue'
+import mycat from './components/MyCat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    helloWorld,
+    mycat
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MyCat.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{ value }}</h2>
+        <h2>{{ count }}</h2>
+        <button @click="count++">say hello</button>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        value:String
+    },
+    data:function(){
+        return{
+            count: 100
+        };
+    }
+}
+</script>
